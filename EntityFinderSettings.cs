2e0db3f398182d9142606e27e12a72934fbf5b66
AyamePafPaf <15815652+aPafPaf@users.noreply.github.com>
2025-07-13 16:01:49 +0300

    #

--- EntityFinderSettings.cs
+++ EntityFinderSettings.cs
@@ -9,9 +9,9 @@ public class EntityFinderSettings : ISettings
     //Mandatory setting to allow enabling/disabling your plugin
     public ToggleNode Enable { get; set; } = new ToggleNode(false);
 
-    [Menu("TransparencySpawnMonsters")]
+    [Menu("Transparency")]
     public RangeNode<int> Transparency { get; set; } = new RangeNode<int>(255, 0, 255);
 
-    [Menu("RadiusSpawnMonsters")]
+    [Menu("Radius")]
     public RangeNode<int> Radius { get; set; } = new RangeNode<int>(30, 0, 255);
 }
